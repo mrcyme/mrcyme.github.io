347a348b727bb025e247c8ba79cdfd7d0f9e1ff2
simeon <simeonmcl@gmail.com>
2022-04-26 13:28:40 +0200

    demo branch

--- leda/static/js/constants.js
+++ leda/static/js/constants.js
@@ -1,3 +1,3 @@
 const action_name = "utter_greet";
-const rasa_server_url = "https://5584-2a02-2788-224-308-6146-615d-97da-8c25.ngrok.io/webhooks/rest/webhook";
+const rasa_server_url =  "https://524c-2a02-2788-224-308-f593-41cc-590c-f1b1.ngrok.io/webhooks/rest/webhook";
 const sender_id = uuidv4();
