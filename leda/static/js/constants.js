7784df0b5899e2543d905a5d52bafb8afea166e2
simeon <simeonmcl@gmail.com>
2022-05-04 10:59:54 +0200

    added 404.html

--- leda/static/js/constants.js
+++ leda/static/js/constants.js
@@ -1,4 +1,4 @@
 const action_name = "utter_greet";
-//const rasa_server_url =  "https://b28d-164-92-158-37.eu.ngrok.io/webhooks/rest/webhook";
-const rasa_server_url =  "http://0.0.0.0:5005/webhooks/rest/webhook";
+const rasa_server_url =  "https://b28d-164-92-158-37.eu.ngrok.io/webhooks/rest/webhook";
+//const rasa_server_url =  "http://0.0.0.0:5005/webhooks/rest/webhook";
 const sender_id = uuidv4();
